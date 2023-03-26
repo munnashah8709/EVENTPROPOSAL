677b55bcd9cdd4d06740289b09dc264eba792c02
munna shah <munnashah8709@gmail.com>
2023-03-27 03:18:16 +0530

    nothing changes

--- eventapp/src/Components/Login/Home.js
+++ eventapp/src/Components/Login/Home.js
@@ -4,7 +4,6 @@ import "../style/home.css"
 import { toast } from 'react-toastify';
 import axios from 'axios';
 
- import party from '../../image/PartyImg.png'
 
 const Home = () => {
   //vendorlogin data
@@ -21,10 +20,6 @@ const Home = () => {
   const [Loginmessage,SetLoginmessage]=useState("");
 
   const navigate=useNavigate();
-
-  
-
-
 
   //Vendorlogin 
   const VendorLogin= async()=>{
@@ -89,12 +84,8 @@ const Home = () => {
       navigate("/")
     }
 
-   
-   
-     
   return (
 <>
-<div className='bgimg' style={{ backgroundImage: `url(${party})`}}>
 <div className="conatiner1" style={{ display:"flex", float:"right", backgroundColor:"white", marginTop:"150px", marginRight:"200px",}}>
 <div className="tab-content" style={{padding:"20px", justifyContent:"center", width:"382px"}}  >
 {/* userbutton and vendor butoon */}
@@ -196,8 +187,7 @@ const Home = () => {
 
   </div>
   </div>
-</div>
-      </>
+</>
   )
 }
 
